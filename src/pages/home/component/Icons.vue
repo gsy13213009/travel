09d4b75c4d04106099061ca67483268c36216e49
Siyi Guo <siyi.guo@grabtaxi.com>
2019-11-23 23:15:19 +0800

    首页图标的swiper:

--- src/pages/home/component/Icons.vue
+++ src/pages/home/component/Icons.vue
@@ -1,25 +1,89 @@
 <template>
   <div class="icons">
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content" src='http://www.zgjyjn.net/uploadfile/2014/0511/thumb_104_134_20140511105236409.jpg' />
-      </div>
-      <p class="icon-desc">热门景点</p>
-    </div>
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="page of pages" class="wrapper">
+        <div class="icon" v-for="item of page">
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.url" />
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+
   </div>
 </template>
 
 <script>
 export default {
-  name: "HomeIcons"
+  name: "HomeIcons",
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        // 前8个放在page[0]里面
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    },
+  },
+  data: function () {
+    return {
+      swiperOption: {
+        autoplay: false
+      },
+      iconList: [
+        {
+          url: "http://www.zgjyjn.net/uploadfile/2014/0511/thumb_104_134_20140511105236409.jpg",
+          desc: "热门景点哈哈哈哈哈哈哈"
+        },
+        {
+          url: "http://www.zgjyjn.net/uploadfile/2014/0511/thumb_104_134_20140511105236409.jpg",
+          desc: "热门景点"
+        },
+        {
+          url: "http://www.zgjyjn.net/uploadfile/2014/0511/thumb_104_134_20140511105236409.jpg",
+          desc: "热门景点"
+        },
+        {
+          url: "http://www.zgjyjn.net/uploadfile/2014/0511/thumb_104_134_20140511105236409.jpg",
+          desc: "热门景点"
+        },
+        {
+          url: "http://www.zgjyjn.net/uploadfile/2014/0511/thumb_104_134_20140511105236409.jpg",
+          desc: "热门景点"
+        },
+        {
+          url: "http://www.zgjyjn.net/uploadfile/2014/0511/thumb_104_134_20140511105236409.jpg",
+          desc: "热门景点"
+        },
+        {
+          url: "http://www.zgjyjn.net/uploadfile/2014/0511/thumb_104_134_20140511105236409.jpg",
+          desc: "热门景点"
+        },
+        {
+          url: "http://www.zgjyjn.net/uploadfile/2014/0511/thumb_104_134_20140511105236409.jpg",
+          desc: "热门景点"
+        },
+        {
+          url: "http://www.zgjyjn.net/uploadfile/2014/0511/thumb_104_134_20140511105236409.jpg",
+          desc: "热门景点"
+        },
+      ]
+    }
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
   @import '~styles/varibles.styl'
+  @import '~styles/mixins.styl'
   .icons
     overflow: hidden
-    background: green
     padding-bottom: 50%
     height: 0
     .icon
@@ -29,7 +93,6 @@ export default {
       height: 0
       padding-bottom: 25%
       float: left
-      background: red
       .icon-img
         position: absolute
         top: 0
@@ -38,7 +101,6 @@ export default {
         bottom: .44rem
         box-sizing: border-box
         padding: .1rem
-        background: blue
         .icon-img-content
           height: 100%
           display: block
@@ -52,4 +114,5 @@ export default {
         line-height: .44rem
         text-align: center
         color: $darkTextColor
+        ellipsis()
 </style>
