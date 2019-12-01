23256892aae25c702025c0f9f5f8e5027fa23daf
Siyi Guo <siyi.guo@grabtaxi.com>
2019-12-01 23:37:07 +0800

    更新list组件的代码

--- src/pages/city/List.vue
+++ src/pages/city/List.vue
@@ -1,10 +1,20 @@
 <template>
-  <div class='list'>
+  <div class='list' ref="wrapper">
     <div class='area'>
-      <div class='title border-topbottom'>A</div>
+      <div class='title border-topbottom'>当前城市</div>
+      <div class="button-list">
+        <div class="button-wrapper">
+          <div class="button">{{currentCity}}</div>
+        </div>
+      </div>
     </div>
     <div class='area'>
-      <div class='title border-topbottom'>A</div>
+      <div class='title border-topbottom'>热门城市</div>
+      <div class="button-list">
+        <div class="button-wrapper" v-for="item of hot_citys">
+          <div class="button">{{item}}</div>
+        </div>
+      </div>
     </div>
     <div class='area'>
       <div class='title border-topbottom'>A</div>
@@ -13,22 +23,54 @@
 </template>
 
 <script>
-export default {
-  name: 'List'
-}
+  export default {
+    name: 'List',
+    data () {
+      return {
+        currentCity: 'fdafds',
+        hot_citys: ['afdsaf', 'dsafds', '北京', '防静电']
+      }
+    }
+  }
 </script>
 
-<style lang='stylus' scoped>
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
   .border-topbottom
     &:before
-      border-color: #777
+      border-color: #ccc
     &:after
       border-color: #ccc
-  .title
-    line-height: .44rem
-    background: #eee
-    padding-left: .2rem
-    color: #666
-    font-size: .26rem
-
+  .border-bottom
+    &:before
+      border-color: #ccc
+  .list
+    overflow: hidden
+    position: absolute
+    top: 1.58rem
+    left: 0
+    right: 0
+    bottom: 0
+    .title
+      line-height: .54rem
+      background: #eee
+      padding-left: .2rem
+      color: #666
+      font-size: .26rem
+    .button-list
+      overflow: hidden
+      padding: .1rem .6rem .1rem .1rem
+      .button-wrapper
+        float: left
+        width: 33.33%
+        .button
+          margin: .1rem
+          padding: .1rem 0
+          text-align: center
+          border: .02rem solid #ccc
+          border-radius: .06rem
+    .item-list
+      .item
+        line-height: .76rem
+        padding-left: .2rem
 </style>
